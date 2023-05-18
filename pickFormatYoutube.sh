2d10602e2f0267fc0a4bd42c46c6c4be2abd0a60
IAT <israel24961@hotmail.com>
2023-05-18 20:28:51 +0200

    Update the yt-dlp program and check whether it exists

--- pickFormatYoutube.sh
+++ pickFormatYoutube.sh
@@ -1,34 +1,49 @@
-#!/bin/bash
+#!/bin/sh
 
 #Format list without the 3 lines
-FormatList=`youtube-dl --list-formats $1 |sed 1,3d`
+
+
+
+FormatList=$(yt-dlp --list-formats "$1" |sed 1,3d)
+
+#Check dependencies
+if ! command -v 'yt-dlp'; then
+    echo "No yt-dlp program"
+    exit 1
+fi
+#Valid arguments:
+if [ -z "$1" ]
+then
+    echo "No url provided"
+    exit 1
+fi
 
 #Ask for video
-VideoFormat=`echo "$FormatList"|grep video|dmenu -p "Pick Video Format" -l 20|cut -d ' ' -f1`
+VideoFormat=$(echo "$FormatList"|grep video|dmenu -p "Pick Video Format" -l 20|cut -d ' ' -f1)
 
-if [ -z $VideoFormat ] #Logs no picked
+if [ -z "$VideoFormat" ] #Logs no picked
 then
     echo "No video format chosen"
 fi
 
 #Ask for audio
-AudioFormat=`echo "$FormatList"|grep audio|dmenu -p "Pick Audio Format" -l 20|cut -d ' ' -f1`
+AudioFormat=$(echo "$FormatList"|grep audio|dmenu -p "Pick Audio Format" -l 20|cut -d ' ' -f1)
 
-if [ -z $AudioFormat ] #Logs no picked
+if [ -z "$AudioFormat" ] #Logs no picked
 then
     echo "No audio format chosen"
 else
-    if [ ! -z $VideoFormat ]
+    if [ -n "$VideoFormat" ]
     then
-        VideoFormat+="+"
+        VideoFormat="$VideoFormat+"
     fi
 fi
 
 
-if [ -z $AudioFormat ] && [ -z $VideoFormat ] #If neither are chosen executes default (Best quality)
+if [ -z "$AudioFormat" ] && [ -z "$VideoFormat" ] #If neither are chosen executes default (Best quality)
 then
-    mpv --force-window=immediate $1
+    mpv --force-window=immediate "$1"
 else
-    mpv --ytdl-format="$VideoFormat$AudioFormat" --force-window=immediate $1
+    mpv --ytdl-format="$VideoFormat$AudioFormat" --force-window=immediate "$1"
 fi
 
